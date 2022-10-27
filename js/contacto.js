0c2e8a28e033361e5cd4e418839a1e3bccdbbf2e
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-10-27 14:25:31 -0400

    Cambios de contact

--- js/contacto.js
+++ js/contacto.js
@@ -1,41 +1,34 @@
-let $botoon = document.getElementById("registrojs")
-let form = document.getElementById("formulari")
-$botoon.addEventListener('click', registrar)
-
-
-
-
+let $boton = document.getElementById("registrojs");
+$boton.addEventListener("click", registrar);
 
 function registrar(event) {
-    event.preventDefault()
-    let datosDeCliente = {
-        nombre: "",
-        apellido: "",
-        mail: "",
-        telefono: 0,
-        nombreDeMascota: "",
-        tipoDeMascota: "",
-        comentario: ""
-    }
-    datosDeCliente.nombre = document.querySelector("#nombre").value
-    datosDeCliente.apellido = document.querySelector("#apellido").value
-    datosDeCliente.mail = document.querySelector("#email").value
-    datosDeCliente.telefono = document.querySelector("#telefono").value
-    datosDeCliente.nombreDeMascota = document.querySelector("#nombreMascota").value
-    datosDeCliente.tipoDeMascota = [...document.querySelectorAll('input[type="radio"]:checked')].map(event => event.value)
-    datosDeCliente.comentario = document.querySelector("#validationTextarea").value
-    
-    let datosDeClienteArry = JSON.parse(localStorage.getItem("datos"))
-    datosDeClienteArry.push(datosDeCliente)
-    localStorage.setItem("datos", JSON.stringify(datosDeClienteArry)) 
-    
-    swal({
-        text: "se guardaron los datos correctamente",
-        icon: "success",
-})
-form.reset();
+  event.preventDefault();
+  let datosDeCliente = {
+    nombre: "",
+    apellido: "",
+    mail: "",
+    telefono: 0,
+    nombreDeMascota: "",
+    tipoDeMascota: "",
+    comentario: "",
+  };
+  datosDeCliente.nombre = document.querySelector("#nombre").value;
+  datosDeCliente.apellido = document.querySelector("#apellido").value;
+  datosDeCliente.mail = document.querySelector("#email").value;
+  datosDeCliente.telefono = document.querySelector("#telefono").value;
+  datosDeCliente.nombreDeMascota = document.querySelector("#nombreMascota").value;
+  datosDeCliente.tipoDeMascota = [...document.querySelectorAll('input[type="radio"]:checked')].map(
+    event => event.value
+  );
+  datosDeCliente.comentario = document.querySelector("#validationTextarea").value;
+
+  let datosDeClienteArry = JSON.parse(localStorage.getItem("datos"));
+  datosDeClienteArry.push(datosDeCliente);
+  localStorage.setItem("datos", JSON.stringify(datosDeClienteArry));
+
+  swal({
+    text: "se guardaron los datos correctamente",
+    icon: "success",
+  });
+  form.reset();
 }
-
-
-
-
